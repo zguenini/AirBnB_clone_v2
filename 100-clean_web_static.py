8c28589ae396ff6d6535e46f0f225213521d5c65
zguenini <zouhair.guenini@gmail.com>
2024-05-04 21:37:26 +0200

    0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['54.157.187.234', '54.86.141.81']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.160.77.90', '10.25.190.21']
+env.hosts = ['54.157.187.234', '54.86.141.81']
 
 
 def do_pack():
